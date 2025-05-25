711c10342af639967aed79194b63f0a72693d6f1
kofiadam1 <save.electrics@gmail.com>
2025-05-25 09:40:43 +0000

    Delete App/Page.tsx

--- App/Page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Welcome to GreatVision Investment</h1>
-      <p>Total Investment: GHS 0.00</p>
-      <p>Total Profit: GHS 0.00</p>
-    </main>
-  );
-}
