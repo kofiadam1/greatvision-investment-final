a0a82e29c145f9b8f5100d6f5ea7ad460a0d7da5
kofiadam1 <save.electrics@gmail.com>
2025-05-25 08:50:47 +0000

    Create Page.tsx

---
+++ App/Page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to GreatVision Investment</h1>
+      <p>Total Investment: GHS 0.00</p>
+      <p>Total Profit: GHS 0.00</p>
+    </main>
+  );
+}
